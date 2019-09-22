440abe71a088d3881d4c43e2ecf480d70ff6bdf6
TOURE NATHAN <toure21@live.com>
2019-09-22 17:43:30 +0300

    move the static resources to util file

--- lib/category_screen.dart
+++ lib/category_screen.dart
@@ -13,6 +13,7 @@ import 'api.dart';
 import 'backdrop.dart';
 import 'category.dart';
 import 'converter_screen.dart';
+import 'util.dart';
 
 final _backgroundColor = Colors.green[100];
 
@@ -36,52 +37,9 @@ class _CategoryScreenState extends State<CategoryScreen> {
   Category _currentCategory;
   final _categories = <Category>[];
 
-  static const _baseColors = <ColorSwatch>[
-    ColorSwatch(0xFF6AB7A8, {
-      'highlight': Color(0xFF6AB7A8),
-      'splash': Color(0xFF0ABC9B),
-    }),
-    ColorSwatch(0xFFFFD28E, {
-      'highlight': Color(0xFFFFD28E),
-      'splash': Color(0xFFFFA41C),
-    }),
-    ColorSwatch(0xFFFFB7DE, {
-      'highlight': Color(0xFFFFB7DE),
-      'splash': Color(0xFFF94CBF),
-    }),
-    ColorSwatch(0xFF8899A8, {
-      'highlight': Color(0xFF8899A8),
-      'splash': Color(0xFFA9CAE8),
-    }),
-    ColorSwatch(0xFFEAD37E, {
-      'highlight': Color(0xFFEAD37E),
-      'splash': Color(0xFFFFE070),
-    }),
-    ColorSwatch(0xFF81A56F, {
-      'highlight': Color(0xFF81A56F),
-      'splash': Color(0xFF7CC159),
-    }),
-    ColorSwatch(0xFFD7C0E2, {
-      'highlight': Color(0xFFD7C0E2),
-      'splash': Color(0xFFCA90E5),
-    }),
-    ColorSwatch(0xFFCE9A9A, {
-      'highlight': Color(0xFFCE9A9A),
-      'splash': Color(0xFFF94D56),
-      'error': Color(0xFF912D2D),
-    }),
-  ];
 
-  final _iconLocation = <String>[
-    'assets/icons/length.png',
-    'assets/icons/area.png',
-    'assets/icons/volume.png',
-    'assets/icons/mass.png',
-    'assets/icons/time.png',
-    'assets/icons/digital_storage.png',
-    'assets/icons/power.png',
-    'assets/icons/currency.png',
-  ];
+
+
 
   //We use didChangeDependencies() so that we can
   //wait for our JSON asset to be loaded in (async).
@@ -112,8 +70,8 @@ class _CategoryScreenState extends State<CategoryScreen> {
           data[key].map<Unit>((dynamic data) => Unit.fromJson(data)).toList();
       var category = Category(
         name: key,
-        color: _baseColors[categoryIndex],
-        iconLocation: _iconLocation[categoryIndex],
+        color: Util.baseColors[categoryIndex],
+        iconLocation: Util.iconLocation[categoryIndex],
         units: units,
       );
       setState(() {
@@ -128,7 +86,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
 
   Future<void> _retrieveApiCategories() async {
     setState(() {
-      _categories.add(Category(name: 'Currency', color: _baseColors.last, units: [], iconLocation: _iconLocation.last));
+      _categories.add(Category(name: 'Currency', color: Util.baseColors.last, units: [], iconLocation: Util.iconLocation.last));
     });
     final jsonUnits = await Api().getUnits('currency');
     // If the API errors out or we have no internet connection, this category
@@ -143,8 +101,8 @@ class _CategoryScreenState extends State<CategoryScreen> {
         _categories.add(Category(
           name: 'Currency',
           units: units,
-          color: _baseColors.last,
-          iconLocation: _iconLocation.last,
+          color: Util.baseColors.last,
+          iconLocation: Util.iconLocation.last,
         ));
       });
     }
